8a8a285cd3b82e9f4548387b12b30d6bb688bc17
j4sysiak <j4sysiak@gmail.com>
2019-03-09 08:10:51 +0100

    1

---
+++ Threads12 -  Semaphores/src/App.java
@@ -0,0 +1,38 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class App {
+
+	public static void main(String[] args) throws Exception {
+		
+	  // Semaphore sem = new Semaphore(0);
+		
+	  //  sem.release();
+	  //  System.out.println("Available permits: " + sem.availablePermits());
+		
+	  // sem.acquire();  // to jest jak lock 
+      // System.out.println("Available permits: " + sem.availablePermits());
+
+		
+		 
+		
+		ExecutorService executor = Executors.newCachedThreadPool();
+		
+		for(int i=0; i<200; i++){
+			executor.submit(new Runnable() {
+				
+				@Override
+				public void run() {
+
+					Connection.getInstance().connect();
+					
+				}
+			});
+		}
+		executor.shutdown();
+		executor.awaitTermination(1, TimeUnit.DAYS);
+	}
+
+}
